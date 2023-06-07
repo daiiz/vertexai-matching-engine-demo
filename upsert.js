adf3c8eee7840a249890bf221f71f19e8ab08191
daiiz <da.iz.mg@gmail.com>
2023-06-07 23:51:44 +0900

    書いていく

---
+++ upsert.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
+const { GoogleAuth } = require("google-auth-library");
+const { genEmbedding } = require("./tools/gen-embedding");
+const {
+  SERVICE_ACCOUNT,
+  INDEX_ENDPOINT_NAME,
+  INDEX_ENDPOINT_PUBLIC_DOMAIN_NAME,
+  INDEX_ID,
+} = process.env;
+
+const rawInputText = process.argv[2];
+
+if (!rawInputText) {
+  console.error("Usage: node upsert.js <text>");
+  process.exit(1);
+}
+
+const auth = new GoogleAuth({
+  scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+  credentials: JSON.parse(SERVICE_ACCOUNT || "{}"),
+});
+
+async function main() {
+  console.log("newText:", rawInputText);
+  // await findNeighbors(rawInputText);
+}
+
+main();

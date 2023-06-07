266e6d425098aaeaa2dfeb4ab23072f59f29e0b6
daiiz <da.iz.mg@gmail.com>
2023-06-07 16:27:14 +0900

    add create-stream-index

---
+++ create-stream-index.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  "https://us-central1-aiplatform.googleapis.com/v1/projects/$(gcloud config get project)/locations/us-central1/indexes" \
+  -d @metadata/my-demo-index3.json

8d4e0c16f3d7edf34db46ce72f221c52a2b7c1f9
daiiz <da.iz.mg@gmail.com>
2023-06-07 12:40:39 +0900

    書いていく

---
+++ search.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+// まv1beta1にしか存在しない
+const { MatchServiceClient } = require("@google-cloud/aiplatform").v1beta1;
+
+const { PROJECT_ID, LOCATION, SERVICE_ACCOUNT, INDEX_ENDPOINT } = process.env;
+
+const clientOptions = {
+  apiEndpoint: "us-central1-aiplatform.googleapis.com",
+  credentials: JSON.parse(SERVICE_ACCOUNT),
+};
+const client = new MatchServiceClient(clientOptions);
+
+async function findNeighbors() {
+  console.log(".......", client);
+}
+
+async function main() {
+  await findNeighbors();
+}
+
+main();

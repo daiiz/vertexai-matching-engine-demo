d31eddbe35c04f5e50a945d61f9e1893bb7f629b
daiiz <da.iz.mg@gmail.com>
2023-06-08 00:35:39 +0900

    impl upsertDatapoints

--- upsert.js
+++ upsert.js
@@ -2,12 +2,7 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const { GoogleAuth } = require("google-auth-library");
 const { genEmbedding } = require("./tools/gen-embedding");
-const {
-  SERVICE_ACCOUNT,
-  INDEX_ENDPOINT_NAME,
-  INDEX_ENDPOINT_PUBLIC_DOMAIN_NAME,
-  INDEX_ID,
-} = process.env;
+const { SERVICE_ACCOUNT, INDEX_NAME } = process.env;
 
 const rawInputText = process.argv[2];
 
@@ -26,11 +21,48 @@ const auth = new GoogleAuth({
  * Upsert with restricts
  * https://cloud.google.com/vertex-ai/docs/matching-engine/update-rebuild-index#upsert_with_restricts
  */
-async function upsertDatapoint(text) {}
+async function upsertDatapoints(texts = []) {
+  const token = await auth.getAccessToken();
+  const apiUri = `https://us-central1-aiplatform.googleapis.com/v1/${INDEX_NAME}:upsertDatapoints`;
+
+  const datapoints = [];
+  for (const text of texts) {
+    datapoints.push({
+      datapoint_id: text,
+      feature_vector: await genEmbedding(text, true),
+      restricts: [
+        { namespace: "appname", allow_list: ["demo"] },
+        { namespace: "username", allow_list: ["daiiz"] },
+        {
+          namespace: "visible",
+          allow_list: [
+            // 驚いているものはプライベート
+            text.endsWith("!") || text.endsWith("！") ? "private" : "public",
+          ],
+        },
+      ],
+    });
+  }
+
+  const res = await fetch(apiUri, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ datapoints }),
+  });
+
+  if (res.ok) {
+    console.log("OK!", await res.json());
+  } else {
+    console.error(res.status, await res.text());
+  }
+}
 
 async function main() {
   console.log("newText:", rawInputText);
-  // await findNeighbors(rawInputText);
+  await upsertDatapoints([rawInputText]);
 }
 
 main();
